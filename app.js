841e0f219fbdb87891ae9c6cc0dfc82deac3d28d
hebahzmd <>
2021-05-05 12:44:28 +0000

    learn

---
+++ app.js
@@ -0,0 +1,23 @@
+var age = prompt('how old are?');
+console.log(age);
+
+if (age < 12)
+{
+  document.write ('<img src="https://upload.3dlat.com/uploads/3dlat.com_13908051911.gif" width="200px"');
+}
+
+else  
+{
+  alert("hello");
+
+   var sex = prompt('are you man or women?');
+console.log(age);
+}
+ if (sex=='women')
+{
+ document.write ('<img src="https://assets.vogue.com/photos/5d5419178c966f000871275b/master/w_2560%2Cc_limit/00-story.jpg" width="200px"');
+} 
+else
+{
+   document.write ('<img src="https://fashionista.com/.image/t_share/MTQ4MTI3OTEyOTM4MTg2MjYy/hp-paris-fashion-week-mens-spring-2018-street-style.jpg" width="200px"');
+}
